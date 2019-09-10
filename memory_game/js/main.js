c7987cfd2ba618533cb79678ff78101da025edb5
Midhat Nashar <medhat.nashar@gmail.com>
2019-09-10 16:43:03 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,18 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+alert('Hello, friends.');
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+var cardOne = cards[1];
+var cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardsInPlay);
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay[cardsInPlay.length - 1]);
+
+if (cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+	}
+	else {
+	alert("Sorry, try again.");
+}
+	}
